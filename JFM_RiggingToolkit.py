6c318aaafac63b37ee5720dae8f9259b497dc326
lemonyfreshness <sethmeshko@hotmail.com>
2026-02-16 18:18:46 -0700

    Updates to unreal naming convention logic

--- JFM_RiggingToolkit.py
+++ JFM_RiggingToolkit.py
@@ -6,6 +6,7 @@ fingerChain = 'thumb'
 auxJointType = 'deform'
 RLSideAbbreviation = None
 FBSideAbbreviation = None
+RLSideAbbreviationFingers = 'l'
 
 
 def makeBufferJoint(*args):
@@ -15,30 +16,41 @@ def makeBufferJoint(*args):
     cmds.parent(joint, bufferJoint)
     cmds.parent(bufferJoint, jointParent)
 
+
 def orientJointForUnreal(*args):
     jointForOrient = cmds.ls(selection = True)[0]
     cmds.makeIdentity(jointForOrient, apply = True, translate = True, rotate = True, scale = True, jointOrient = True)
     print('orientJointForUnreal(), jointForOrient is: ' + str(jointForOrient))
     reorientChildrenValue = cmds.checkBox(reorientChildren, query=True, value=True)
+    reverseXValue = cmds.checkBox(reverseX, query=True, value=True)
     print('orientJointForUnreal(), reorientChildrenValue is: ' + str(reorientChildrenValue))
-    cmds.joint(jointForOrient, exists = True, orientJoint = 'xzy', secondaryAxisOrient = 'xdown',  autoOrientSecondaryAxis = True,  zeroScaleOrient = True) #children = reorientChildrenValue,
+    print('orientJointForUnreal(), reverseXValue is: ' + str(reverseXValue))
+    cmds.joint(jointForOrient, edit = True, orientJoint = 'xzy', secondaryAxisOrient = 'xdown', children = reorientChildrenValue, zeroScaleOrient = True) #
+    
+    if reverseXValue == True:
+        cmds.joint(jointForOrient, edit = True, orientJoint = 'xzy', secondaryAxisOrient = 'ydown', children = reorientChildrenValue, zeroScaleOrient = True)#  
+        bufferJoint = setupJointForReorient()
+        cmds.rotate(0,180,0, bufferJoint)
+        cleanupReorientedJoint()
+
 
 def parentShapesUnderControl(controlJoint, importedShape):
-	#print('parentShapesUnderControl(), the controlJoint is: ' + str(controlJoint))
-	shapeSelection = cmds.listRelatives(importedShape, children = True, type = 'shape')
-	#print('parentShapesUnderControl(), the shapeSelection is: ' + str(shapeSelection))
-	i = 0
-	for eachShape in shapeSelection:
-		renamedShape = cmds.rename(eachShape, controlJoint + 'shape_' + str(i))
-		cmds.parent(renamedShape, controlJoint, relative = True, shape = True)
-		i = i+1
-	#print('parentShapesUnderControl(), the controlJoint is: ' + str(controlJoint))
-	
-	importedShapeChildren = cmds.listRelatives(importedShape, allDescendents = True)
-	#print('parentShapesUnderControl(), the importedShapeChildren is: ' + str(importedShapeChildren))
-	if importedShapeChildren == None:
-		cmds.delete(importedShape)
-	return controlJoint
+
+    #print('parentShapesUnderControl(), the controlJoint is: ' + str(controlJoint))
+    shapeSelection = cmds.listRelatives(importedShape, children = True, type = 'shape')
+    #print('parentShapesUnderControl(), the shapeSelection is: ' + str(shapeSelection))
+    i = 0
+    for eachShape in shapeSelection:
+        renamedShape = cmds.rename(eachShape, controlJoint + 'shape_' + str(i))
+        cmds.parent(renamedShape, controlJoint, relative = True, shape = True)
+        i = i+1
+    #print('parentShapesUnderControl(), the controlJoint is: ' + str(controlJoint))
+    
+    importedShapeChildren = cmds.listRelatives(importedShape, allDescendents = True)
+    #print('parentShapesUnderControl(), the importedShapeChildren is: ' + str(importedShapeChildren))
+    if importedShapeChildren == None:
+        cmds.delete(importedShape)
+    return controlJoint
 
 
 def renameBodyJoints(*args):
@@ -84,31 +96,42 @@ def renameFingerJoints(*args):
     jointSelection = cmds.ls(selection = True)[0]
     jointChildren = cmds.listRelatives(jointSelection, allDescendents = True, type= 'joint')
     jointChildren.reverse()
-    jointChildren.insert(0, jointSelection)
-    print('renameFingerJoints(), the jointChildren are: ' + str(jointChildren))
+    jointChildren.insert(0, jointSelection)  
     hasMetacarpalsValue = cmds.checkBox(hasMetacarpals, query=True, value=True)
     hasEndJointValue = cmds.checkBox(hasEndJoint, query=True, value=True)
-    jointChildrenLength = len(jointChildren)
+    isToesValue = cmds.checkBox(isToes, query=True, value=True)
 
+    print('renameFingerJoints(), the hasEndJointValue is : ' + str(hasEndJointValue))
+    
     fingerChain
     if hasMetacarpalsValue == True:
-         metacarpal = jointChildren.pop([0])
-         cmds.rename(metacarpal, fingerChain + '_metacarpal')
-
-    i = 1
-    for eachChild in jointChildren:
         name = fingerChain
+        metacarpal = jointChildren.pop(0)
+        name = name + '_metacarpal'
+        if RLSideAbbreviationFingers != None:
+            name = name + '_' + RLSideAbbreviationFingers
+        cmds.rename(metacarpal, name)
+
+    jointChildrenLength = len(jointChildren)
+    print('renameFingerJoints(), the jointChildrenLength are: ' + str(jointChildrenLength))
+    i = 1
+    ai = 1
+    for eachChild in jointChildren:
+        print('renameFingerJoints(), i is : ' + str(i))
+        name = fingerChain
+        if isToesValue == True:
+            name = name + 'toe' 
         name = name + '_' + str(0) + str(i)
-        if RLSideAbbreviation != None:
-            name = name + '_' + RLSideAbbreviation
-        if i == jointChildrenLength and hasEndJointValue == True:
+        if RLSideAbbreviationFingers != None:
+            name = name + '_' + RLSideAbbreviationFingers
+        if ai == jointChildrenLength and hasEndJointValue == True:
             name = name + '_end'
         cmds.rename(eachChild, name)
         if hasEndJointValue == True and i != jointChildrenLength -1:
             i = i+1
         elif hasEndJointValue == False:
             i = i+1
-        
+        ai = ai+1
      
 
 def setupController(*args):
@@ -128,48 +151,51 @@ def setupReorientJoint(*args):
 
 
 def setupJointForReorient():
-	selection = cmds.ls(selection = True)
-	if selection != None and len(selection) == 1:
-		selection = selection[0]
-		print('dupeJointForReorient(), the selection is:' + selection)
-		if cmds.objectType(selection) == 'joint':
-			selectionParent = cmds.listRelatives(selection, parent = True)
-			selectionChildren = cmds.listRelatives(selection, children = True)
-			tempNull = cmds.group(empty = True, world = True, name = selection + '_TempNull')
-			cmds.parent(tempNull, selectionParent)
-			if selectionChildren != None:
-				cmds.select(clear = True)
-				cmds.parent(selectionChildren, tempNull)
-			dupedJoint = cmds.duplicate(selection, parentOnly = True, name = selection + '_ReorientJoint')
-			cmds.parent(selection, dupedJoint)
-			cmds.select(dupedJoint)
-		else:
-			print('setupJointForReorient, Select a JOINT dumbass!')
-	else:
-		print('setupJointForReorient, Select ONE JOINT you want to reorient')
+    selection = cmds.ls(selection = True)
+    if selection != None and len(selection) == 1:
+        selection = selection[0]
+        print('dupeJointForReorient(), the selection is: ' + selection)
+        if cmds.objectType(selection) == 'joint':
+            selectionParent = cmds.listRelatives(selection, parent = True)
+            selectionChildren = cmds.listRelatives(selection, children = True)
+            tempNull = cmds.group(empty = True, world = True, name = selection + '_TempNull')
+            if selectionParent == None:
+                cmds.parent(tempNull, world = True)
+            else:
+                cmds.parent(tempNull, selectionParent)
+        if selectionChildren != None:
+            cmds.select(clear = True)
+            cmds.parent(selectionChildren, tempNull)
+        dupedJoint = cmds.duplicate(selection, parentOnly = True, name = selection + '_ReorientJoint')
+        cmds.parent(selection, dupedJoint)
+        cmds.select(dupedJoint)
+        return dupedJoint
+    else:
+        print('setupJointForReorient, Select a JOINT dumbass!')
 
 
 
 def cleanupReorientedJoint():
-	selection = cmds.ls(selection = True)
-	if selection != None and len(selection) == 1:
-		selection = selection[0]
-		print('cleanupReorientedJoint(), The selection is: ' + selection)
-		if '_ReorientJoint' in selection:
-			jointForFreezeTransforms = selection
-			cmds.makeIdentity(jointForFreezeTransforms, apply = True, translate = True, rotate = True, scale = True)
-			originalJoint = cmds.listRelatives(selection, children = True)
-			originalJoint = originalJoint[0]
-			originalParent = cmds.listRelatives(originalJoint + '_TempNull', parent = True)
-			reparentChildren = cmds.listRelatives(originalJoint + '_TempNull', children = True)
-			if reparentChildren != None:
-				cmds.parent(reparentChildren, originalJoint)
-			cmds.parent(originalJoint, originalParent)
-			cmds.delete(jointForFreezeTransforms, originalJoint + '_TempNull')
-		else:
-			print('cleanupReorientedJoint(), Select the _ReorientJoint')
-					
-	print('cleanupReorientedJoint(), Select ONE JOINT you want to reorient')
+    selection = cmds.ls(selection = True)
+    if selection != None and len(selection) == 1:
+        selection = selection[0]
+        print('cleanupReorientedJoint(), The selection is: ' + selection)
+        if '_ReorientJoint' in selection:
+            jointForFreezeTransforms = cmds.listRelatives(selection, children = True)
+            cmds.makeIdentity(jointForFreezeTransforms, apply = True, translate = True, rotate = True, scale = True, jointOrient = True)
+            originalJoint = cmds.listRelatives(selection, children = True)[0]
+            originalParent = cmds.listRelatives(originalJoint + '_TempNull', parent = True)
+            reparentChildren = cmds.listRelatives(originalJoint + '_TempNull', children = True)
+            if reparentChildren != None:
+                    cmds.parent(reparentChildren, originalJoint)
+            if originalParent != None:
+                cmds.parent(originalJoint, originalParent)
+            else:
+                cmds.parent(originalJoint, world = True)
+            cmds.delete(selection, originalJoint + '_TempNull')
+        else:
+            print('cleanupReorientedJoint(), Select the _ReorientJoint')
+
 
 
 def importObject(nameOfFile):
@@ -204,6 +230,7 @@ def setBodyRegionName(name):
     bodyRegion = name
     print('setBodyRegionName(), the bodyRegoin is:' + name)
 
+
 def setFingerChainName(name):
     global fingerChain
     fingerChain = name
@@ -234,6 +261,18 @@ def setRLSide(RLsideOfBody):
     print('setRLSide(), the sideAbbriviation is:' + str(RLSideAbbreviation))
 
 
+def setRLSideFinger(RLsideOfBody):
+    global RLSideAbbreviationFingers
+    if RLsideOfBody == 'left':
+         RLSideAbbreviationFingers = 'l'
+    if RLsideOfBody == 'right':
+         RLSideAbbreviationFingers = 'r'
+    if RLsideOfBody == 'none':
+         RLSideAbbreviationFingers = None
+    print('setRLSide(), the RLsideOfBody is:' + RLsideOfBody)
+    print('setRLSide(), the RLSideAbbreviationFingers is:' + str(RLSideAbbreviationFingers))
+
+
 
 #___________________________________________Start GUI___________________________________________
 
@@ -256,6 +295,7 @@ cmds.button(label = 'Setup/Clean Joint For Reorient', command = setupReorientJoi
 
 cmds.rowColumnLayout(numberOfColumns=2, columnWidth=[(1,400), (2,400)], columnOffset=[(1,'both', 3)])
 reorientChildren = cmds.checkBox(label='Reorient Children', value=False)
+reverseX = cmds.checkBox(label='reverseX (leg left)', value=False)
 cmds.button(label = 'Orient Joint for Unreal (Left)', command = orientJointForUnreal)
 cmds.setParent('..')
 
@@ -284,6 +324,7 @@ tabChild3 = cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1,800)], colum
 cmds.separator(h=10, style='none')
 cmds.text('Rename Unreal Body joints for, Select the joint(s) in order, set region and hit button')
 cmds.separator(h=10, style='single')
+
 cmds.rowColumnLayout(numberOfColumns=4, columnWidth=[(1,200), (2,200), (3,200), (4,200)], columnOffset=[(1,'both', 3)])
 rightLeftSide = cmds.optionMenu(label='Right/Left Side', changeCommand=setRLSide)
 cmds.menuItem(label='none')
@@ -315,7 +356,7 @@ isDynamicJointCheckbox = cmds.checkBox(label='dynamic joint', value=False)
 isAttachJointCheckbox = cmds.checkBox(label='attach joint', value=False)
 cmds.setParent('..')
 
-cmds.separator(h=10, style='single')
+cmds.separator(h=10, style='double')
 
 cmds.rowColumnLayout(numberOfColumns=2, columnWidth=[(1,400), (2,400)], columnOffset=[(1,'both', 3)])
 bodyRegion = cmds.optionMenu(label='Body Region', changeCommand=setBodyRegionName)
@@ -329,22 +370,39 @@ cmds.menuItem(label='hand')
 cmds.menuItem(label='pectoral')
 cmds.menuItem(label='deltoid')
 cmds.menuItem(label='elbow')
+cmds.menuItem(label='thigh')
+cmds.menuItem(label='knee')
+cmds.menuItem(label='calf')
+cmds.menuItem(label='foot')
+cmds.menuItem(label='ball')
 cmds.button(label = 'Rename Joint', command = renameBodyJoints)
 cmds.setParent('..')
 
+cmds.separator(h=10, style='double')
+
 cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1,800)], columnOffset=[(1,'both', 3)])
-cmds.text('rename for Unreal hand joints, select the first joint in the chain, check whether it has metacarpals and end joints and hit button')
+cmds.text('Rename for Unreal hand joints, select the first joint in the chain, check whether it has metacarpals and end joints and hit button')
 cmds.setParent('..')
 
-cmds.rowColumnLayout(numberOfColumns=2, columnWidth=[(1,400), (2,400)], columnOffset=[(1,'both', 3)])
-hasMetacarpals = cmds.checkBox(label='Has Metacarpals', value=True)
-hasEndJoint = cmds.checkBox(label='Has End Jont', value=True)
+cmds.rowColumnLayout(numberOfColumns=5, columnWidth=[(1,140), (2,140), (3,140), (4,140), (5,140)], 
+                     columnSpacing=[(1, 10), (2, 10), (3, 10), (4, 10), (5, 10)], columnOffset=[(1,'both', 3)])
 fingerChain = cmds.optionMenu(label='Finger Chain', changeCommand=setFingerChainName)
 cmds.menuItem(label='thumb')
 cmds.menuItem(label='index')
 cmds.menuItem(label='middle')
 cmds.menuItem(label='ring')
 cmds.menuItem(label='pinky')
+
+rightLeftSideFingers = cmds.optionMenu(label='Right/Left Side', changeCommand=setRLSideFinger)
+cmds.menuItem(label='left')
+cmds.menuItem(label='right')
+cmds.menuItem(label='none')
+hasMetacarpals = cmds.checkBox(label='Has Metacarpals', value=True)
+hasEndJoint = cmds.checkBox(label='Has End Jont', value=True)
+isToes = cmds.checkBox(label='Are Toes', value=True)
+cmds.setParent('..')
+
+cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1,800)], columnOffset=[(1,'both', 3)])
 cmds.button(label = 'Rename Finger Joints', command = renameFingerJoints)
 cmds.setParent('..')
 
